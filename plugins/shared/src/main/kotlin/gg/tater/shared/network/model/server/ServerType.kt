3114b3ea070a58ad44041f716cb9ac01b1628aa4
ThePotatoDev <potatocoding@gmail.com>
2025-02-05 16:41:56 -0600

    Update tab

--- plugins/shared/src/main/kotlin/gg/tater/shared/network/model/server/ServerType.kt
+++ plugins/shared/src/main/kotlin/gg/tater/shared/network/model/server/ServerType.kt
@@ -6,6 +6,6 @@ enum class ServerType(val spawn: WrappedPosition) {
 
     SERVER(WrappedPosition(0.0, 101.0, 0.0, 45F, -1.7F)),
     SPAWN(WrappedPosition(-8.0, 67.0, 13.0, 45F, -1.7F)),
-    LIMBO(WrappedPosition(0.0, 1.0, 0.0, 45F, -1.7F))
+    LIMBO(WrappedPosition(0.0, 1.0, 0.0, 45F, -1.7F)),
 
 }
---
+++ plugins/shared/src/main/kotlin/gg/tater/shared/warp/WarpType.kt
@@ -0,0 +1,24 @@
+package gg.tater.shared.warp
+
+import gg.tater.shared.player.position.WrappedPosition
+import me.lucko.helper.item.ItemStackBuilder
+
+enum class WarpType(val icon: ItemStackBuilder, val position: WrappedPosition) {
+
+//    PVP(
+//        ItemStackBuilder.of(Material.DIAMOND_SWORD)
+//            .name("&cPvP &7(Click)"),
+//        WrappedPosition(),
+//    ),
+//    SPAWN(
+//        ItemStackBuilder.of(Material.REDSTONE_LAMP)
+//            .name("&3Spawn &7(Click)"),
+//        WrappedPosition()
+//    ),
+//    PLANETS(
+//        ItemStackBuilder.of(Material.END_STONE)
+//            .name("&bPlanets &7(Click)"),
+//        WrappedPosition()
+//    )
+
+}
