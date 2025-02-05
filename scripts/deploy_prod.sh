48104627c40f926d4ae084c762e96232c8fde574
ThePotatoDev <potatocoding@gmail.com>
2025-02-04 20:24:58 -0600

    Prod development setup

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Define the base directory
-BASE_DIR=$(pwd)
+# Get the current directory where the terminal was opened
+PROJECT_ROOT="$(pwd)"
 
-# Define the services
-SERVICES=("server" "proxy" "spawn" "limbo")
+# Wait for 2 seconds
+sleep 2
 
-# Loop through each service and apply the fleet.yaml configuration
-for SERVICE in "${SERVICES[@]}"; do
-    cd "$BASE_DIR/servers/$SERVICE/src/main/helm"
-    kubectl apply -f fleet.yaml
-done
+# Apply Kubernetes configurations
+cd "$PROJECT_ROOT/servers/server/src/main/helm"
+kubectl apply -f fleet_prod.yaml
+
+cd "$PROJECT_ROOT/servers/proxy/src/main/helm"
+kubectl apply -f fleet_prod.yaml
+
+cd "$PROJECT_ROOT/servers/spawn/src/main/helm"
+kubectl apply -f fleet_prod.yaml
+
+cd "$PROJECT_ROOT/servers/limbo/src/main/helm"
+kubectl apply -f fleet_prod.yaml
